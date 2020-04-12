7f19cd44aa453cf7c2ac7bdfa5b675720695d31f
Настенька <nastenka@Alexandrs-MacBook-Pro.local>
2020-04-12 15:08:10 +0300

    First try

---
+++ iOS/Views/StartView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Views;
+using TrackYourRide.Core.ViewModels;
+using UIKit;
+
+namespace TrackYourRide.iOS.Views
+{
+    public partial class StartView : MvxViewController<StartViewModel>
+    {
+        public StartView() : base("StartView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
+
